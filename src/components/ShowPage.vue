465ff9e7664898cf8606eb41460462d741ee0a44
79479334 <79479334@qq.com>
2021-01-03 17:19:36 +0800

    main

--- src/components/ShowPage.vue
+++ src/components/ShowPage.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="b_page">
     <div class="up_user">
-      <div class="user_lable">
-        <a-icon type="user" />
-        Aldnoah
+      <div class="leftButton" @click="back">
+        <a-icon type="left" />
+      </div>
+      <div style="display: flex">
+        <div class="warning" v-if="netaData.wordShield">
+          <a-icon type="warning" />
+        </div>
+        <div class="user_lable">
+          <a-icon type="user" />
+          Aldnoah
+        </div>
       </div>
     </div>
     <div class="cyxq">
       <div class="wenzi">
-        <ruby> 
-          {{netaData.netaWord}}
-          <rt>{{netaData.pinyin}}</rt>
-          </ruby>
+        <ruby>
+          {{ netaData.netaWord }}
+          <rt>{{ netaData.pinyin }}</rt>
+        </ruby>
       </div>
       <div class="katakana">
         <span> 假名注音: </span>
-        <span> {{netaData.katakana}} </span>
+        <span> {{ netaData.katakana }} </span>
       </div>
     </div>
     <a-divider />
@@ -26,8 +34,11 @@
       <a-tag color="#108ee9"> #108ee9 </a-tag>
     </div>
     <a-divider />
-     <div v-if="netaData.netaDate">出现日期：{{parseInt(netaData.netaDate/100)}} 年 {{parseInt(netaData.netaDate%100)}} 月 </div>
-     <div v-if="!netaData.netaDate">出现日期：不明 </div>
+    <div v-if="netaData.netaDate">
+      出现日期：{{ parseInt(netaData.netaDate / 100) }} 年
+      {{ parseInt(netaData.netaDate % 100) }} 月
+    </div>
+    <div v-if="!netaData.netaDate">出现日期：不明</div>
     <a-divider />
     <div>
       <a-collapse v-model="activeKey">
@@ -55,22 +66,22 @@
 export default {
   data() {
     return {
-      netaData:{},
-      activeKey: ["1","2"],
+      netaData: {},
+      activeKey: ["1", "2"],
     };
   },
-  created(){
-    let id=this.$route.query.netaId;
+  created() {
+    let id = this.$route.query.netaId;
     this.$axios({
-      method:"get",
-      url:"http://localhost:8080/neta/selectFullNeta",
-      params:{
-        netaId:id,
+      method: "get",
+      url: "http://localhost:8080/neta/selectFullNeta",
+      params: {
+        netaId: id,
       },
-    }).then((response)=>{
-      if(response.data.resultCode==1){
-         console.log(response)
-        this.netaData=response.data.resultData;
+    }).then((response) => {
+      if (response.data.resultCode == 1) {
+        console.log(response);
+        this.netaData = response.data.resultData;
       }
     });
   },
@@ -79,9 +90,11 @@ export default {
       console.log(key);
     },
   },
-  methods:{
-
-  }
+  methods: {
+    back(){
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 <style>
@@ -95,15 +108,46 @@ export default {
 }
 .up_user {
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   width: 100%;
+  height: 3em;
+}
+.leftButton {
+  margin-right: 0.5em;
+  font-weight: bold;
+  text-align: center;
+  font-size: 1em;
+  line-height: 2em;
+  width: 2.2em;
   height: 2em;
+  border: 1px solid #d9d9d9;
+  border-style: none solid solid solid;
+  border-radius: 0 0 5px 5px;
+}
+.leftButton:hover{
+  cursor: pointer;
+  color: #40a9ff;
+  border: 1px solid#40a9ff;
+  border-style: none solid solid solid;
+}
+.warning {
+  margin-right: 0.5em;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  font-size: 1em;
+  line-height: 2em;
+  width: 2em;
+  height: 2em;
+  background-color: #d63031;
+  border-radius: 0 0 5px 5px;
 }
 .user_lable {
   color: white;
   font-weight: bold;
   text-align: center;
   font-size: 1em;
+  height: 2em;
   line-height: 2em;
   width: 10em;
   background-color: #f39c12;
